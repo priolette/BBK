ead14d6e202ea3688d7b93af1050e0c6ce9a6240
MantasJan <mantasjankunas@outlook.com>
2024-05-05 19:30:46 +0300

    [PSK-21] Added initial migration.

---
+++ bbk-backend/BBK.API/Data/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace BBK.API.Data.Models;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public int RecipeId { get; set; }
+    public required string CreatedById { get; set; }
+    public required DateTimeOffset CreatedAt { get; set; }
+    public required string Text { get; set; }
+
+    public virtual Recipe Recipe { get; set; } = default!;
+}
