9a93be1e2ab658ac2ec054f7fce749a13065db94
Domas Dragunas <dragunasdomas@gmail.com>
2024-05-13 00:39:12 +0300

    Fetching data from BE

--- bbk-frontend/app/page.tsx
+++ bbk-frontend/app/page.tsx
@@ -6,24 +6,39 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Recipe } from "@/types/Recipe";
 import Image from "next/image";
-import { dummyRecipes } from "@/data/recipes";
 import Link from "next/link";
 
-export default function Home() {
+async function getRecipes() {
+  const getRecipesPath = process.env.API_PATH + 'recipes';
+
+  const res = await fetch(getRecipesPath);
+  if (!res.ok) {
+    //what solution for errors ? Message component or some kind of notification
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+
+export default async function Home() {
+  const dummyImage = "https://handletheheat.com/wp-content/uploads/2017/03/Chewy-Brownies-Square-1.jpg";
+  const dummyAuthor = "Comming soon";
+  const recipes = await getRecipes()
   return (
     <main className="flex flex-auto gap-4 p-4">
-      {dummyRecipes.map((recipe) => (
+      {recipes.data.map((recipe: Recipe) => (
         <Link href={`/${recipe.id}`} key={recipe.id}>
           <Card key={recipe.id} className="">
             <CardHeader>
-              <CardTitle>{recipe.name}</CardTitle>
+              <CardTitle>{recipe.title}</CardTitle>
               <CardDescription>{recipe.description}</CardDescription>
             </CardHeader>
             <CardContent className="px-0">
               <Image
-                src={recipe.image}
-                alt={recipe.name}
+                src={dummyImage}
+                alt={dummyImage}
                 width={0}
                 height={0}
                 sizes="100vw"
@@ -31,7 +46,7 @@ export default function Home() {
               />
             </CardContent>
             <CardFooter>
-              <CardDescription>Created by: {recipe.author}</CardDescription>
+              <CardDescription>Created by: {dummyAuthor}</CardDescription>
             </CardFooter>
           </Card>
         </Link>
