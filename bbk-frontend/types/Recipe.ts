ff6066123d2acbfb0c8b61db0b946be93e03faa2
Adomas Volungevičius <volungevicius.adomas@gmail.com>
2024-05-23 13:42:21 +0300

    Fix formatting

--- bbk-frontend/types/Recipe.ts
+++ bbk-frontend/types/Recipe.ts
@@ -1,9 +1,8 @@
 export type Recipe = {
-    id: number;
-    title: string;
-    description: string;
-    createdById: string;
-    createdAt: string;
-    modifiedAt: string | null;
-  };
-  
+  id: number;
+  title: string;
+  description: string;
+  createdById: string;
+  createdAt: string;
+  modifiedAt: string | null;
+};
